c9adc23b8c55c52ada74338222fd108e28ad64cb
dimasstudent <dimas2023@student.unas.ac.id>
2026-01-01 12:23:46 +0700

    feat: implement product management features with CRUD operations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,53 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @category = params[:category]
     @products = Product.all
+  end
+
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+     @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status:
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :new, status:
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :size_start, :size_end, :category, :price, :discount, :rating)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
